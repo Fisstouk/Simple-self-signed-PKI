69174e3acb693125b2b9738f39fe5f8830a45ac3
Fisstouk <90407928+Fisstouk@users.noreply.github.com>
2023-12-08 18:58:40 +0100

    Update script to work in current directory

--- script_pki.sh
+++ script_pki.sh
@@ -3,11 +3,11 @@
 set -e
 set -x
 #
-signing_ca_client_conf="/home/${USER}/pki_conf_files/signing-ca-client.conf"
-signing_ca_server_conf="/home/${USER}/pki_conf_files/signing-ca-server.conf"
+signing_ca_client_conf="pki_conf_files/signing-ca-client.conf"
+signing_ca_server_conf="pki_conf_files/signing-ca-server.conf"
 openssl_cmd="/usr/bin/openssl"
-root_ca_conf="/home/${USER}/pki_conf_files/root-ca.conf"
-server_conf="/home/${USER}/pki_conf_files/server.conf"
+root_ca_conf="pki_conf_files/root-ca.conf"
+server_conf="pki_conf_files/server.conf"
 
 if [[ ! -f "${openssl_cmd}" ]]; then
 	echo '"${openssl_cmd}" does not exist, install in progress...'
@@ -19,32 +19,32 @@ fi
 
 echo "Create ca, crl and certs directories in your /home/ directory"
 sleep 2
-mkdir -p ~/ca/root-ca/private ca/root-ca/db crl certs-client certs-server
+mkdir -p ca/root-ca/private ca/root-ca/db crl certs-client certs-server
 
 echo "Secure root-ca directory permissions"
 sleep 2
-chmod 700 ~/ca/root-ca/private
+chmod 700 ca/root-ca/private
 
 echo "Create database for maintaining CA"
 sleep 2
-cp /dev/null ~/ca/root-ca/db/root-ca.db
-cp /dev/null ~/ca/root-ca/db/root-ca.db.attr
-echo 01 >~/ca/root-ca/db/root-ca.crt.srl
-echo 01 >~/ca/root-ca/db/root-ca.crl.srl
+cp /dev/null ca/root-ca/db/root-ca.db
+cp /dev/null ca/root-ca/db/root-ca.db.attr
+echo 01 >ca/root-ca/db/root-ca.crt.srl
+echo 01 >ca/root-ca/db/root-ca.crl.srl
 
 echo "Create a CA request"
 sleep 2
 openssl req -new \
-	-out ~/ca/root-ca.csr \
+	-out ca/root-ca.csr \
 	-config "${root_ca_conf}" \
-	-keyout ~/ca/root-ca/private/root-ca.key
+	-keyout ca/root-ca/private/root-ca.key
 
 echo "Create selfsign CA certificate"
 sleep 2
 openssl ca -selfsign \
 	-config "${root_ca_conf}" \
-	-in ~/ca/root-ca.csr \
-	-out ~/ca/root-ca.crt \
+	-in ca/root-ca.csr \
+	-out ca/root-ca.crt \
 	-extensions root_ca_ext
 
 # Create Signing CA or Intermediate CA Client
@@ -59,24 +59,24 @@ chmod 700 ca/signing-ca-client/private
 
 echo "Create database for maintaining CA"
 sleep 2
-cp /dev/null ~/ca/signing-ca-client/db/signing-ca-client.db
-cp /dev/null ~/ca/signing-ca-client/db/signing-ca-client.db.attr
-echo 01 >~/ca/signing-ca-client/db/signing-ca-client.crt.srl
-echo 01 >~/ca/signing-ca-client/db/signing-ca-client.crl.srl
+cp /dev/null ca/signing-ca-client/db/signing-ca-client.db
+cp /dev/null ca/signing-ca-client/db/signing-ca-client.db.attr
+echo 01 >ca/signing-ca-client/db/signing-ca-client.crt.srl
+echo 01 >ca/signing-ca-client/db/signing-ca-client.crl.srl
 
 echo "Create CA request"
 sleep 2
 openssl req -new \
 	-config "${signing_ca_client_conf}" \
-	-out ~/ca/signing-ca-client.csr \
-	-keyout ~/ca/signing-ca-client/private/signing-ca-client.key
+	-out ca/signing-ca-client.csr \
+	-keyout ca/signing-ca-client/private/signing-ca-client.key
 
 echo "Create CA certificate"
 sleep 2
 openssl ca \
 	-config "${root_ca_conf}" \
-	-in ~/ca/signing-ca-client.csr \
-	-out ~/ca/signing-ca-client.crt \
+	-in ca/signing-ca-client.csr \
+	-out ca/signing-ca-client.crt \
 	-extensions signing_ca_ext
 
 # Signing CA
@@ -92,7 +92,7 @@ echo "Create Certificate revocation list (CRL)"
 sleep 2
 openssl ca -gencrl \
 	-config "${signing_ca_client_conf}" \
-	-out ~/crl/signing-ca-client.crl
+	-out crl/signing-ca-client.crl
 
 # Output
 
@@ -100,17 +100,17 @@ echo "Create PKCS#12 bundle for Prof"
 sleep 2
 openssl pkcs12 -export \
 	-name "Esgi" \
-	-inkey ~/certs-client/esgi.local.key \
-	-in ~/certs-client/esgi.local.crt \
-	-out ~/certs-client/prof.esgi.local.p12
+	-inkey certs-client/esgi.local.key \
+	-in certs-client/esgi.local.crt \
+	-out certs-client/prof.esgi.local.p12
 
 echo "Create PKCS#12 bundle for Eleve"
 sleep 2
 openssl pkcs12 -export \
 	-name "Esgi" \
-	-inkey ~/certs-client/esgi.local.key \
-	-in ~/certs-client/esgi.local.crt \
-	-out ~/certs-client/eleve.esgi.local.p12
+	-inkey certs-client/esgi.local.key \
+	-in certs-client/esgi.local.crt \
+	-out certs-client/eleve.esgi.local.pkcs12
 
 # Create Signing CA or Intermediate CA Server
 
@@ -124,24 +124,24 @@ chmod 700 ca/signing-ca-server/private
 
 echo "Create database for maintaining CA"
 sleep 2
-cp /dev/null ~/ca/signing-ca-server/db/signing-ca-server.db
-cp /dev/null ~/ca/signing-ca-server/db/signing-ca-server.db.attr
-echo 01 >~/ca/signing-ca-server/db/signing-ca-server.crt.srl
-echo 01 >~/ca/signing-ca-server/db/signing-ca-server.crl.srl
+cp /dev/null ca/signing-ca-server/db/signing-ca-server.db
+cp /dev/null ca/signing-ca-server/db/signing-ca-server.db.attr
+echo 01 >ca/signing-ca-server/db/signing-ca-server.crt.srl
+echo 01 >ca/signing-ca-server/db/signing-ca-server.crl.srl
 
 echo "Create CA request"
 sleep 2
 openssl req -new \
 	-config "${signing_ca_server_conf}" \
-	-out ~/ca/signing-ca-server.csr \
-	-keyout ~/ca/signing-ca-server/private/signing-ca-server.key
+	-out ca/signing-ca-server.csr \
+	-keyout ca/signing-ca-server/private/signing-ca-server.key
 
 echo "Create CA certificate"
 sleep 2
 openssl ca \
 	-config "${root_ca_conf}" \
-	-in ~/ca/signing-ca-server.csr \
-	-out ~/ca/signing-ca-server.crt \
+	-in ca/signing-ca-server.csr \
+	-out ca/signing-ca-server.crt \
 	-extensions signing_ca_ext
 
 # Signing CA
@@ -151,15 +151,15 @@ sleep 2
 SAN=DNS:www.esgi.local \
 	openssl req -new \
 	-config "${server_conf}" \
-	-out ~/certs-server/esgi.local.csr \
-	-keyout ~/certs-server/esgi.local.key
+	-out certs-server/esgi.local.csr \
+	-keyout certs-server/esgi.local.key
 
 echo "Create TLS server certificate"
 sleep 2
 openssl ca \
 	-config "${signing_ca_server_conf}" \
-	-in ~/certs-server/esgi.local.csr \
-	-out ~/certs-server/esgi.local.crt \
+	-in certs-server/esgi.local.csr \
+	-out certs-server/esgi.local.crt \
 	-extensions server_ext
 
 # Revoke Certificate
@@ -173,4 +173,4 @@ echo "Create Certificate revocation list (CRL)"
 sleep 2
 openssl ca -gencrl \
 	-config "${signing_ca_server_conf}" \
-	-out ~/crl/signing-ca-server.crl
+	-out crl/signing-ca-server.crl
